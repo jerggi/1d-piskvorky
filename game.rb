97be79d70bcb99704eb81e0b244bac1661b36f70
Petr Blaho <pblaho@redhat.com>
2015-11-04 18:11:00 +0100

    Add simple API for classes Game and Player

--- game.rb
+++ game.rb
@@ -1,2 +1,7 @@
 class Game
+  def initialize(o_player, x_player)
+  end
+
+  def start_game
+  end
 end
--- player.rb
+++ player.rb
@@ -1,2 +1,7 @@
 class Player
+  def play(board, stone)
+    # board je hraci plocha - Array s 20 pozicemi
+    # sign je 'x' nebo 'o'
+    # vrati pozici 0 - 19, kam umisti svuj kamen
+  end
 end
